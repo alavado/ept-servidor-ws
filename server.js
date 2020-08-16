c78b573bc5591695980091e973cc022d4324cb09
alavado <alejandro.lvd@gmail.com>
2020-08-16 01:51:33 +0000

    cambia a wss

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
-const expressWs = require('express-ws')(app)
+
+const https = require('https')
+const fs = require('fs')
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/compsci.cl-0001/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/compsci.cl-0001/fullchain.pem')
+}
+const httpsServer = https.createServer(options, app)
+httpsServer.listen(2304)
+
+const expressWs = require('express-ws')(app, httpsServer)
 
 let ultimoDato = 'no hay nada'
+
+app.get('/', (req, res) => res.send('hola 2304'))
 
 app.ws('/input', (ws, req) => {
   console.log('se conectó una raspberry')
@@ -11,10 +23,10 @@ app.ws('/input', (ws, req) => {
       ws.send(ultimoDato)
     }
     else {
+      console.log('me llegó', msg)
       ultimoDato = msg
       ws.send('ack')
     }
   })
 })
 
-app.listen(2304)
